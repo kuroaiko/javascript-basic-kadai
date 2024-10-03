5215cda4f04215aa5a3a8f2b0429353849a4310c
kuroaiko <knoaiko03745649@gmail.com>
2024-10-03 16:05:14 +0900

    kadai_005をアップロードしました。

---
+++ kadai_005/bmi.js
@@ -0,0 +1,16 @@
+// 体重と身長の変数の宣言
+let weight;
+let height;
+
+// 体重と身長の値の代入
+weight = 68;
+height = 1.7;
+
+// BMI(体格指数)計算式の変数宣言
+let bmi = (weight / (height * height));
+
+// BMIのコンソールへの出力
+console.log(bmi);
+
+
+
